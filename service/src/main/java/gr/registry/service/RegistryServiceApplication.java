0526dae458274d0863caf31b5f3c90c6d9e88ed6
vpantelios <bplemans2k@gmail.com>
2025-08-20 00:21:13 +0300

    fix many issues

--- service/src/main/java/gr/registry/service/RegistryServiceApplication.java
+++ service/src/main/java/gr/registry/service/RegistryServiceApplication.java
@@ -2,9 +2,13 @@ package gr.registry.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /** Εκκίνηση RESTful υπηρεσίας μητρώου πολιτών. */
 @SpringBootApplication(scanBasePackages = "gr.registry")
+@EnableJpaRepositories(basePackages = "gr.registry.service.repo")
+@EntityScan(basePackages = "gr.registry.domain")
 public class RegistryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RegistryServiceApplication.class, args);
